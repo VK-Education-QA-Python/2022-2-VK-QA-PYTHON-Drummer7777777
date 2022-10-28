af29af6ea00a3714913122bc69a755f448e9f180
Drummer7777777 <newlogan95@mail.ru>
2022-10-28 23:55:05 +0500

    done

---
+++ homework3/conftest.py
@@ -0,0 +1,40 @@
+import os.path
+import pytest
+from api.client import ApiClient
+import creds
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', default='chrome')
+    parser.addoption('--url', default='https://target-sandbox.my.com')
+    parser.addoption('--headless', action='store_true')
+    parser.addoption('--debug_log', action='store_true')
+
+@pytest.fixture(scope='session')
+def config(request):
+    browser = request.config.getoption('--browser')
+    url = request.config.getoption('--url')
+    headless = request.config.getoption('--headless')
+    debug_log = request.config.getoption('--debug_log')
+    return {
+        'browser': browser,
+        'url': url,
+        'headless': headless,
+        'debug_log': debug_log,
+    }
+
+@pytest.fixture(scope='session')
+def credentials():
+    return creds.LOGIN, creds.PASSWORD
+
+@pytest.fixture(scope='session')
+def api_client(credentials, config):
+    return ApiClient(base_url = config['url'], login=credentials[0], password=credentials[1])
+
+@pytest.fixture(scope='session')
+def repo_root():
+    return os.path.abspath(os.path.join(__file__, os.path.pardir))
+
+@pytest.fixture()
+def file_path(repo_root):
+    return os.path.join(repo_root, 'files', '90x75_2.jpg')
